76568afc5751f6a3f3f551bfaf025f96eb30b07a
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-10-04 23:03:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,50 @@
-$(document).ready(function () {
-	$('.menu-toggle').click(function () {
-		$('.menu-toggle').toggleClass('menu-open');
-		$('.menu').toggleClass('active');
-	});
-	$('.link').click(function () {
-		$('.menu-toggle').toggleClass('menu-open');
-		$('.menu').toggleClass('active');
-	});
-	$('.imageOne').click(function () {
-		$('.overlayOne').toggleClass('active');
-	});
-	$('.imageTwo').click(function () {
-		$('.overlayTwo').toggleClass('active');
-	});
-	$('.imageThree').click(function () {
-		$('.overlayThree').toggleClass('active');
-	});
-	$('.imageFour').click(function () {
-		$('.overlayFour').toggleClass('active');
-	});
-	$('.imageFive').click(function () {
-		$('.overlayFive').toggleClass('active');
-	});
-	$('.imageSix').click(function () {
-		$('.overlaySix').toggleClass('active');
-	});
-	$('.closeOverlayOne').click(function () {
-		$('.overlayOne').toggleClass('active');
-	});
-	$('.closeOverlayTwo').click(function () {
-		$('.overlayTwo').toggleClass('active');
-	});
-	$('.closeOverlayThree').click(function () {
-		$('.overlayThree').toggleClass('active');
-	});
-	$('.closeOverlayFour').click(function () {
-		$('.overlayFour').toggleClass('active');
-	});
-	$('.closeOverlayFive').click(function () {
-		$('.overlayFive').toggleClass('active');
-	});
-	$('.closeOverlaySix').click(function () {
-		$('.overlaySix').toggleClass('active');
-	});
-});
+document.addEventListener('DOMContentLoaded', function(event) {
+// $(document).ready(function () {
+// 	$('.menu-toggle').click(function () {
+// 		$('.menu-toggle').toggleClass('menu-open');
+// 		$('.menu').toggleClass('active');
+// 	});
+// 	$('.link').click(function () {
+// 		$('.menu-toggle').toggleClass('menu-open');
+// 		$('.menu').toggleClass('active');
+// 	});
+// 	$('.imageOne').click(function () {
+// 		$('.overlayOne').toggleClass('active');
+// 	});
+// 	$('.imageTwo').click(function () {
+// 		$('.overlayTwo').toggleClass('active');
+// 	});
+// 	$('.imageThree').click(function () {
+// 		$('.overlayThree').toggleClass('active');
+// 	});
+// 	$('.imageFour').click(function () {
+// 		$('.overlayFour').toggleClass('active');
+// 	});
+// 	$('.imageFive').click(function () {
+// 		$('.overlayFive').toggleClass('active');
+// 	});
+// 	$('.imageSix').click(function () {
+// 		$('.overlaySix').toggleClass('active');
+// 	});
+// 	$('.closeOverlayOne').click(function () {
+// 		$('.overlayOne').toggleClass('active');
+// 	});
+// 	$('.closeOverlayTwo').click(function () {
+// 		$('.overlayTwo').toggleClass('active');
+// 	});
+// 	$('.closeOverlayThree').click(function () {
+// 		$('.overlayThree').toggleClass('active');
+// 	});
+// 	$('.closeOverlayFour').click(function () {
+// 		$('.overlayFour').toggleClass('active');
+// 	});
+// 	$('.closeOverlayFive').click(function () {
+// 		$('.overlayFive').toggleClass('active');
+// 	});
+// 	$('.closeOverlaySix').click(function () {
+// 		$('.overlaySix').toggleClass('active');
+// 	});
+// });
 
 // Button to scroll up to the top
 
@@ -66,3 +67,4 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+});
