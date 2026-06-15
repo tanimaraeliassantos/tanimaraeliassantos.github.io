112de87bb05247cdaa673cd7f6b287c653dcef4c
Tanimara Elias Santos <tanimaraeliassantos@gmail.com>
2026-06-15 22:38:15 +0200

    feat(router): add routes for home and article views

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { ArticleComponent } from './article-component/article-component';
+import { HomeComponent } from './home-component/home-component';
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'blog/:id', component: ArticleComponent },
+];
